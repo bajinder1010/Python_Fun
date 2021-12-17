5788728ed13de725f86d8c358696d77ab5cdcf7f
Bajinder Singh <bajindersingh@192.168.1.153>
2021-12-17 21:46:01 +0000

    func..

---
+++ func_return_func.py
@@ -0,0 +1,12 @@
+def grumpy():
+    print("I am a grumpy cat:")
+    def no_n_times(n):
+            print("No", n,"times...")
+            def no_m_more_times(m):
+                    print("...and no", m,"more times")
+                    for i in range(n+m):
+                     print("no")
+            return no_m_more_times
+    return no_n_times
+
+grumpy()(4)(2)
