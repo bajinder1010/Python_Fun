15637275110a58759aeadb88a795149d04747f0c
Bajinder Singh <bajindersingh@192.168.1.105>
2022-02-15 19:46:08 +0000

    ds

--- classes_objects/stack.py
+++ classes_objects/stack.py
@@ -15,17 +15,27 @@ class Stack(object):
             self.stack.pop()
     def size(self):
         return len(self.stack)
+    def peek(self):
+        if self.is_empty():
+            return "nothing to peek"
+        else:
+            return self.stack[len(self.stack)-1]
     def prettyprint(self):
         for thing in self.stack[::-1]:
             print('|_',thing,'_|')
     def add_list(self,new_list):
         for elm in new_list:
             self.stack.append(elm)
+    def is_empty(self):
+        return len(self.stack)==0
 
 pancakes = Stack()
 pancakes.add_one("blueberry")
-pancakes.add_many("choclate",4)
+pancakes.add_many("choclate",1)
 print(pancakes.size())
 pancakes.remove_one()
 print(pancakes.size())
 pancakes.prettyprint()
+pancakes.remove_one()
+print(pancakes.peek())
+print(pancakes.is_empty())
